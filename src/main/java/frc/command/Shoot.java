d9a825c2656a5981c4851b8c1bacdbdd68056336
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2024-02-02 16:00:10 -0500

    gralde

--- src/main/java/frc/command/Shoot.java
+++ src/main/java/frc/command/Shoot.java
@@ -2,22 +2,28 @@ package frc.command;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.subsystems.Shooter;
+import frc.subsystems.Indexer;
 
 public class Shoot extends Command{
      private Shooter shooter;
+     private Indexer indexer;
 
-    public Shoot(Shooter shooter){
+    public Shoot(Shooter shooter, Indexer indexer){
         this.shooter = shooter;
-        addRequirements(shooter);
+        this.indexer = indexer;
+        addRequirements(indexer, shooter);
     }
 
     @Override
     public void execute(){
+        indexer.moveToShoot();
         shooter.runShoot();
+        
     }
 
     @Override
     public void end(boolean interrupted){
         shooter.stop();
+        indexer.stop();
     }
 }
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,11 +2,12 @@ package frc.robot;
 
 import java.util.HashMap;
 
-
+import frc.command.*;
 import frc.robot.util.DPadButton;
 import frc.robot.util.DPadButton.Direction;
 
-import frc.subsystems.Drivetrain;
+import frc.subsystems.*;
+import frc.subsystems.Indexer;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -49,11 +50,12 @@ public class OI {
     private Trigger manipRight;
     
     public final Drivetrain drivetrain = new Drivetrain();
-
-
+    
+    public final Indexer larry = new Indexer();
 
     public OI() {
         initControllers();
+        manipBButton.onTrue(new RunIndexer(larry));
 
     }
 
