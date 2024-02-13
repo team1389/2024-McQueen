d1c0b8f38e0602b810d8a75aa7a9da89b6867d73
Ria Gulati <riagulati111@gmail.com>
2024-02-12 19:48:50 -0500

    outtake continued

---
+++ src/main/java/frc/command/RunOuttake.java
@@ -0,0 +1,29 @@
+package frc.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.subsystems.Intake;
+
+public class RunOuttake extends Command{
+    private Intake intake;
+    public RunOuttake(Intake intake){
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void execute(){
+        intake.runOuttake();
+        
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+        intake.stop();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return intake.hitSensor();
+    }
+}
