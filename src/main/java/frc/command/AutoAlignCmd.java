e6ecdec1af2d42d1f9609bc44f33930fa9d705fa
Zach Wheeler <92531055+1352351253@users.noreply.github.com>
2024-03-10 18:40:15 -0400

    dont run shoot without a programmer there

--- src/main/java/frc/command/AutoAlignCmd.java
+++ src/main/java/frc/command/AutoAlignCmd.java
@@ -1,69 +1,69 @@
-// package frc.command;
+package frc.command;
 
-// import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.math.kinematics.SwerveModuleState;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.RobotMap.DriveConstants;
-// import frc.subsystems.Drivetrain;
-// import frc.subsystems.LimelightVision;
-// import frc.util.LimelightHelpers;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotMap.DriveConstants;
+import frc.subsystems.Drivetrain;
+import frc.subsystems.LimelightVision;
+import frc.util.LimelightHelpers;
 
-// public class AutoAlign extends Command{
+public class AutoAlign extends Command{
 
-//     private final Drivetrain drivetrain;
-//     private final LimelightVision limeLightVision;
-//     double tx, ta;
+    private final Drivetrain drivetrain;
+    private final LimelightVision limeLightVision;
+    double tx, ta;
     
-//      public AutoAlign(Drivetrain drivetrain, LimelightVision limeLightVision) {
-//         this.drivetrain = drivetrain;
-//         this.limeLightVision = limeLightVision;
+     public AutoAlign(Drivetrain drivetrain, LimelightVision limeLightVision) {
+        this.drivetrain = drivetrain;
+        this.limeLightVision = limeLightVision;
 
-//         addRequirements(drivetrain, limeLightVision);
-//     }
+        addRequirements(drivetrain, limeLightVision);
+    }
 
 
-//     ///connect to limelight to proceed, thrid link;
-// //https://www.chiefdelphi.com/t/introducing-limelight-lib/425660/66
-// //https://docs.limelightvision.io/docs/docs-limelight/tutorials/tutorial-getting-in-range
-// //https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltag-3d#point-of-interest-tracking
-//     @Override
-//     public void execute() {        
-//         tx = LimelightHelpers.getTX("");
-//         //needs to be fixed. Plug into limelight try and find the best way to find dist through robotPose type
-//         LimelightHelpers.LimelightResults llresults = LimelightHelpers.getLatestResults("");
-//         var rrResults = llresults.targetingResults.targets_Retro[0];
-//         var tx = rrResults.tx;
-//         var ty = rrResults.ty;
-//         var robotPose = rrResults.getTargetPose_RobotSpace();
-//         var tz = robotPose.getZ();        
+    ///connect to limelight to proceed, thrid link;
+//https://www.chiefdelphi.com/t/introducing-limelight-lib/425660/66
+//https://docs.limelightvision.io/docs/docs-limelight/tutorials/tutorial-getting-in-range
+//https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltag-3d#point-of-interest-tracking
+    @Override
+    public void execute() {        
+        tx = LimelightHelpers.getTX("");
+        //needs to be fixed. Plug into limelight try and find the best way to find dist through robotPose type
+        LimelightHelpers.LimelightResults llresults = LimelightHelpers.getLatestResults("");
+        var rrResults = llresults.targetingResults.targets_Retro[0];
+        var tx = rrResults.tx;
+        var ty = rrResults.ty;
+        var robotPose = rrResults.getTargetPose_RobotSpace();
+        var tz = robotPose.getZ();        
         
-//         double speed = 0.1;
+        double speed = 0.1;
 
-//         //math
-//         double targetAngle = 0;
-//         double rotAngle = tx;
+        //math
+        double targetAngle = 0;
+        double rotAngle = tx;
 
-//        // rotAngle = tx;
+       // rotAngle = tx;
 
-//         SmartDashboard.putNumber("Rotation Angle", Math.toDegrees(rotAngle));
-//         SmartDashboard.putNumber("Target Angle", Math.toDegrees(targetAngle));
+        SmartDashboard.putNumber("Rotation Angle", Math.toDegrees(rotAngle));
+        SmartDashboard.putNumber("Target Angle", Math.toDegrees(targetAngle));
 
-//         if (Math.abs(rotAngle) < 0.5) {
-//             drivetrain.stopModules();
-//             //rotAngle*speed
-//         } else {
-//             ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0,
-//                     0, speed * rotAngle);
-//             SwerveModuleState[] moduleStates = DriveConstants.driveKinematics.toSwerveModuleStates(chassisSpeeds);
-//             drivetrain.setModuleStates(moduleStates);
-//         }
-//     }
+        if (Math.abs(rotAngle) < 0.5) {
+            drivetrain.stopModules();
+            //rotAngle*speed
+        } else {
+            ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0,
+                    0, speed * rotAngle);
+            SwerveModuleState[] moduleStates = DriveConstants.driveKinematics.toSwerveModuleStates(chassisSpeeds);
+            drivetrain.setModuleStates(moduleStates);
+        }
+    }
 
-//     @Override
-//     public void end(boolean interrupted) {
-//         drivetrain.stopModules();
-//     }
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.stopModules();
+    }
 
     
-// }
+}
